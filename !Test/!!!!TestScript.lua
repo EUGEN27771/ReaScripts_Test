ebfb8ddde97cbc942dc8c75c28eabf084a409da2
EUGEN27771 <pops27771@gmail.com>
2017-12-24 01:06:35 +0200

    t

--- !Test/!!!!TestScript.lua
+++ !Test/!!!!TestScript.lua
@@ -2,11 +2,13 @@
    * Description: TestScript
    * Author: EUGEN27771
    * Version: 1.00
-   * Provides: Modules/*.{lua}
+   * Provides: 
+        Modules/*.{lua}
+        Images/*.{png}
 --]]
 
 package.path = debug.getinfo(1,"S").source:match[[^@?(.*[\/])[^\/]-$]] .."?.lua;".. package.path
 require "Modules.Module1"
 require "Modules.Module2"
 
-
+-- Provides: Modules/*.{lua} >> reapack-index - OK!
