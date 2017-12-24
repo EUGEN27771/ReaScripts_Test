ffacd09aec957a25b2ba6fc63ae8389b343c0993
EUGEN27771 <pops27771@gmail.com>
2017-12-24 02:56:31 +0200

    t

--- !Test/!!!!TestScript.lua
+++ !Test/!!!!TestScript.lua
@@ -1,10 +1,9 @@
 --[[
    * Description: TestScript
    * Author: EUGEN27771
-   * Version: 1.06
-   * Provides: 
-       Modules/Module1.lua
-       Images/Image1.png
+   * Version: 1.07
+   * Provides: Modules/Module1.lua
+   * Provides: Images/Image1.png
 --]]
 
 package.path = debug.getinfo(1,"S").source:match[[^@?(.*[\/])[^\/]-$]] .."?.lua;".. package.path
