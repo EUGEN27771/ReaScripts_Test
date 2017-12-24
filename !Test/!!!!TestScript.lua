65572327ff62ff798f47634249b90526a132a4e7
EUGEN27771 <pops27771@gmail.com>
2017-12-24 03:07:28 +0200

    е

--- !Test/!!!!TestScript.lua
+++ !Test/!!!!TestScript.lua
@@ -1,10 +1,9 @@
---[[
-   * Description: TestScript
-   * Author: EUGEN27771
-   * Version: 1.07
-   * Provides: Modules/Module1.lua
-   * Provides: Images/Image1.png
---]]
+
+-- Description: TestScript
+-- Author: EUGEN27771
+-- Version: 1.07
+-- @provides
+--   {Images,Modules}/*
 
 package.path = debug.getinfo(1,"S").source:match[[^@?(.*[\/])[^\/]-$]] .."?.lua;".. package.path
 require "Modules.Module1"
